c7c0c70f8eda774206e87d76b3f6bf46609f2b98
Pieter Bruyninckx <pbruyninckx@gmail.com>
2023-10-22 23:18:39 +0200

    Day 12 part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -4,11 +4,11 @@ use std::io;
 use std::io::{BufRead, BufReader, Lines};
 use std::path::Path;
 
-const NUM_RULES: i32 = 32;
+const NUM_RULES: i64 = 32;
 
 #[derive(Debug)]
 struct Rules {
-    produce_sets: Vec<i32>,
+    produce_sets: Vec<i64>,
 }
 
 impl Rules {
@@ -20,14 +20,14 @@ impl Rules {
                 let index = line
                     .chars()
                     .take(5)
-                    .map(|c| (c == '#') as i32)
+                    .map(|c| (c == '#') as i64)
                     .fold(0, |acc, el| 2 * acc + el);
                 produce_sets[index as usize] = 1;
             }
         }
         Ok(Self { produce_sets })
     }
-    fn produce(&self, combo: i32) -> i32 {
+    fn produce(&self, combo: i64) -> i64 {
         self.produce_sets[combo as usize]
     }
 }
@@ -40,37 +40,42 @@ struct Input {
 
 #[derive(Debug, Clone)]
 struct State {
-    state: Vec<i32>,
-    index: i32,
+    state: Vec<i64>,
+    index: i64,
 }
 
 impl State {
     fn from_string(s: &str) -> Self {
         Self {
-            state: s.chars().map(|c| (c == '#') as i32).collect(),
+            state: s.chars().map(|c| (c == '#') as i64).collect(),
             index: 0,
         }
     }
 
-    fn sum_plant_indices(&self) -> i32 {
-        self.state.iter().enumerate().filter(|(_i, val)| val != &&0).map(|(i, _val)| i as i32 + self.index).sum()
+    fn sum_plant_indices(&self) -> i64 {
+        self.state
+            .iter()
+            .enumerate()
+            .filter(|(_i, val)| val != &&0)
+            .map(|(i, _val)| i as i64 + self.index)
+            .sum()
     }
 
     fn next(&self, rules: &Rules) -> Self {
         let numeric = self
             .state
             .iter()
-            .chain(vec![0, 0, 0].iter())
+            .chain([0, 0, 0].iter())
             .fold(vec![0], |mut acc, el| {
                 acc.push((2 * acc.last().unwrap() + *el) % NUM_RULES);
                 acc
             });
-        let next_state = numeric
-            .iter()
-            .map(|v| rules.produce(*v))
-            .skip(1)
-            .collect();
-        State { state: next_state , index: self.index-2}
+        let next_state_it = numeric.iter().map(|v| rules.produce(*v));
+        let num_zeros = next_state_it.clone().take_while(|v| *v == 0).count();
+        State {
+            state: next_state_it.skip(num_zeros).collect(),
+            index: self.index - 3 + (num_zeros as i64),
+        }
     }
 }
 
@@ -82,7 +87,7 @@ fn parse_input(filename: &Path) -> Result<Input, Error> {
         lines
             .next()
             .ok_or(Error::msg("Empty file"))??
-            .rsplit_once(" ")
+            .rsplit_once(' ')
             .ok_or(Error::msg("Invalid input file"))?
             .1,
     );
@@ -97,12 +102,18 @@ fn parse_input(filename: &Path) -> Result<Input, Error> {
     Ok(result)
 }
 
-fn solve(input: &Input) -> i32 {
+fn solve(input: &Input, num_iterations: i64) -> i64 {
     let rules = &input.rules;
     let mut state = input.state.clone();
 
-    for _i in 0..20 {
-        state = state.next(&rules);
+    for i in 0..num_iterations {
+        let old_state = state.clone();
+        state = state.next(rules);
+
+        if old_state.state == state.state {
+            let sum_diff = state.sum_plant_indices() - old_state.sum_plant_indices();
+            return state.sum_plant_indices() + (num_iterations - i - 1) * sum_diff;
+        }
     }
 
     state.sum_plant_indices()
@@ -110,6 +121,9 @@ fn solve(input: &Input) -> i32 {
 
 fn main() -> Result<(), Error> {
     let input = parse_input(Path::new("data/input12.txt"))?;
-    println!("{}", solve(&input));
+    for num_iterations in [20, 50000000000] {
+        println!("{}", solve(&input, num_iterations));
+    }
+
     Ok(())
 }
